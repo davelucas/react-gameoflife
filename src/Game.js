c42fa842ce59379782425c18485d5636686c2485
David Lucas <dlucas@thoughtworks.com>
2018-08-16 18:21:16 +0100

    Call the api at localhost:8080

--- src/Game.js
+++ src/Game.js
@@ -9,17 +9,17 @@ const HEIGHT = 600;
 
 class Cell extends React.Component {
 
-    render() {
-        const { x, y } = this.props;
-        return (
-            <div className="Cell" style={{
-                left: `${CELL_SIZE * x + 1}px`,
-                top: `${CELL_SIZE * y + 1}px`,
-                width: `${CELL_SIZE - 1}px`,
-                height: `${CELL_SIZE - 1}px`,
-            }} />
-        );
-    }
+  render() {
+    const {x, y} = this.props;
+    return (
+      <div className="Cell" style={{
+        left: `${CELL_SIZE * x + 1}px`,
+        top: `${CELL_SIZE * y + 1}px`,
+        width: `${CELL_SIZE - 1}px`,
+        height: `${CELL_SIZE - 1}px`,
+      }}/>
+    );
+  }
 }
 
 
@@ -28,112 +28,123 @@ class Game extends React.Component {
   rows = HEIGHT / CELL_SIZE;
   cols = WIDTH / CELL_SIZE;
 
-    constructor() {
-        super();
-        this.setState({ id: api.newBoard(this.rows, this.cols, []) });
+  componentDidMount() {
+    api.newBoard(this.rows, this.cols, [])
+      .then(response => {
+        this.setState({id: response.id});
+      });
+  }
+
+  state = {
+    id: "",
+    cells: [],
+    isRunning: false,
+    interval: 100,
+  };
+
+  getElementOffset = () => {
+    const rect = this.boardRef.getBoundingClientRect();
+    const doc = document.documentElement;
+
+    return {
+      x: (rect.left + window.pageXOffset) - doc.clientLeft,
+      y: (rect.top + window.pageYOffset) - doc.clientTop,
+    };
+  };
+
+  handleClick = (event) => {
+
+    const elemOffset = this.getElementOffset();
+    const offsetX = event.clientX - elemOffset.x;
+    const offsetY = event.clientY - elemOffset.y;
+
+    const x = Math.floor(offsetX / CELL_SIZE);
+    const y = Math.floor(offsetY / CELL_SIZE);
+
+    api.toggle(this.state.id, x, y).then(cells => this.setState({cells: cells}));
+  };
+
+  runGame = () => {
+    this.setState({isRunning: true});
+    this.runIteration();
+  };
+
+  stopGame = () => {
+    this.setState({isRunning: false});
+    if (this.timeoutHandler) {
+      window.clearTimeout(this.timeoutHandler);
+      this.timeoutHandler = null;
     }
+  };
 
-    state = {
-        id: "",
-        cells: [],
-        isRunning: false,
-        interval: 100,
-    };
+  runIteration() {
+    api.nextGeneration(this.state.id)
+      .then(cells => this.setState({cells: cells}));
 
-    getElementOffset = () => {
-        const rect = this.boardRef.getBoundingClientRect();
-        const doc = document.documentElement;
+    this.timeoutHandler = window.setTimeout(() => {
+      this.runIteration();
+    }, this.state.interval);
+  }
 
-        return {
-            x: (rect.left + window.pageXOffset) - doc.clientLeft,
-            y: (rect.top + window.pageYOffset) - doc.clientTop,
-        };
-    };
+  handleIntervalChange = (event) => {
+    this.setState({interval: event.target.value});
+  };
 
-    handleClick = (event) => {
-
-        const elemOffset = this.getElementOffset();
-        const offsetX = event.clientX - elemOffset.x;
-        const offsetY = event.clientY - elemOffset.y;
-        
-        const x = Math.floor(offsetX / CELL_SIZE);
-        const y = Math.floor(offsetY / CELL_SIZE);
-
-        this.setState({ cells: api.toggle(this.state.id, x, y) });
-    };
-
-    runGame = () => {
-        this.setState({ isRunning: true });
-        this.runIteration();
-    };
-
-    stopGame = () => {
-        this.setState({ isRunning: false });
-        if (this.timeoutHandler) {
-            window.clearTimeout(this.timeoutHandler);
-            this.timeoutHandler = null;
-        }
-    };
-
-    runIteration() {
-        this.setState({ cells: api.nextGeneration(this.state.id) });
-
-        this.timeoutHandler = window.setTimeout(() => {
-            this.runIteration();
-        }, this.state.interval);
-    }
-
-    handleIntervalChange = (event) => {
-        this.setState({ interval: event.target.value });
-    };
-
-    handleClear = () => {
+  handleClear = () => {
+    api.newBoard(this.rows, this.cols, []).then(response => {
       this.setState({
         cells: [],
-        id: api.newBoard(this.rows, this.cols, [])
+        id: response.id
       });
-    };
+    })
+  };
 
-    handleRandom = () => {
-        let cells = []
-        for (let y = 0; y < this.rows; y++) {
-            for (let x = 0; x < this.cols; x++) {
-                if (Math.random() >= 0.8) cells.push({x:x, y:y})
-            }
-        }
+  handleRandom = () => {
+    let cells = [];
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        if (Math.random() >= 0.8) cells.push({x: x, y: y})
+      }
+    }
 
+    api.newBoard(this.rows, this.cols, cells).then(response => {
       this.setState({
         cells: cells,
-        id: api.newBoard(this.rows, this.cols, cells)
+        id: response.id
       });
-    };
+    });
+  };
 
-    render() {
-        const { cells, interval, isRunning } = this.state;
-        return (
-            <div>
-                <div className="Board"
-                    style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}
-                    onClick={this.handleClick}
-                    ref={(n) => { this.boardRef = n; }}>
+  render() {
+    const {cells, interval, isRunning} = this.state;
+    return (
+      <div>
+        <div className="Board"
+             style={{width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}
+             onClick={this.handleClick}
+             ref={(n) => {
+               this.boardRef = n;
+             }}>
 
-                    {cells.map(cell => (
-                        <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`}/>
-                    ))}
-                </div>
+          {
+            cells.map(cell => (
+              <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`}/>
+            ))
+          }
+        </div>
 
-                <div className="controls">
-                    Update every <input value={interval} onChange={this.handleIntervalChange} /> msec
-                    {isRunning ?
-                        <button className="button" onClick={this.stopGame}>Stop</button> :
-                        <button className="button" onClick={this.runGame}>Run</button>
-                    }
-                    <button className="button" onClick={this.handleRandom}>Random</button>
-                    <button className="button" onClick={this.handleClear}>Clear</button>
-                </div>
-            </div>
-        );
-    }
+        <div className="controls">
+          Update every <input value={interval} onChange={this.handleIntervalChange}/> msec
+          {isRunning ?
+            <button className="button" onClick={this.stopGame}>Stop</button> :
+            <button className="button" onClick={this.runGame}>Run</button>
+          }
+          <button className="button" onClick={this.handleRandom}>Random</button>
+          <button className="button" onClick={this.handleClear}>Clear</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 
--- src/api.js
+++ src/api.js
@@ -1,84 +1,56 @@
 
-let tempBoard = [];
-let tempRows = 0;
-let tempCols = 0;
-
-function makeCells(board) {
-  let cells = [];
-  for (let y = 0; y < tempRows; y++) {
-    for (let x = 0; x < tempCols; x++) {
-      if (board[y][x]) {
-        cells.push({ x, y });
-      }
+export const newBoard = (rows, columns, cells) =>
+  fetch('/api/game/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    body: JSON.stringify({
+      rows: rows,
+      columns: columns,
+      activeCells: cells
+    })
+  }).then((response) => {
+    if (response.status !== 201) {
+      throw new Error("Something went wrong creating the game");
     }
-  }
+    return response.json();
+  }).catch((error) =>
+    console.log("Unable to create a new game")
+  );
 
-  return cells;
-}
 
-function makeBoard(cells){
-  let board = [];
-  for (let y = 0; y < tempRows; y++) {
-    board[y] = [];
-    for (let x = 0; x < tempCols; x++) {
-      board[y][x] = false;
+export const toggle = (gameId, x, y) =>
+  fetch(`/api/game/${gameId}/toggleCell`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    body: JSON.stringify({
+      x: x,
+      y: y
+    })
+  }).then((response) => {
+    if (response.status !== 200) {
+      throw new Error("Something went wrong toggling cell {x},{y}");
     }
-  }
+    return response.json();
+  }).catch((error) =>
+    console.log("Problem toggling a cell")
+  );
 
-  for (const cell of cells) {
-    board[cell.y][cell.x] = true;
-  }
-
-  return board;
-}
-
-export function newBoard(rows, columns, cells) {
-  tempCols = columns;
-  tempRows = rows;
-  tempBoard = makeBoard(cells);
-  return "temp-game"
-}
-
-export function toggle(gameId, x, y) {
-  if (x >= 0 && x <= tempCols && y >= 0 && y <= tempRows) {
-    tempBoard[y][x] = !tempBoard[y][x];
-  }
-
-  return makeCells(tempBoard);
-}
-
-export function nextGeneration(gameId) {
-  let newCells = [];
-
-  for (let y = 0; y < tempRows; y++) {
-    for (let x = 0; x < tempCols; x++) {
-      let neighbors = calculateNeighbors(tempBoard, x, y);
-      if (tempBoard[y][x]) {
-        if(neighbors === 2 || neighbors === 3) newCells.push({x:x,y:y})
-      } else {
-        if (!tempBoard[y][x] && neighbors === 3) {
-          newCells.push({x:x,y:y})
-        }
-      }
+export const nextGeneration = (gameId) =>
+  fetch(`/api/game/${gameId}/nextGeneration`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
     }
-  }
-
-  tempBoard = makeBoard(newCells);
-  return newCells;
-}
-
-function calculateNeighbors(board, x, y) {
-  let neighbors = 0;
-  const dirs = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
-  for (let i = 0; i < dirs.length; i++) {
-    const dir = dirs[i];
-    let y1 = y + dir[0];
-    let x1 = x + dir[1];
-
-    if (x1 >= 0 && x1 < tempCols && y1 >= 0 && y1 < tempRows && board[y1][x1] ) {
-      neighbors++;
+  }).then((response) => {
+    if (response.status !== 200) {
+      throw new Error(`Something went wrong getting the next generation of ${gameId}`);
     }
-  }
+    return response.json();
+  }).catch((error) =>
+    console.log("Problem getting next generation")
+  );
 
-  return neighbors;
-}
